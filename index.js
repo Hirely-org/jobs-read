f6956dd7de549f4c13dba948d0f6c2f36d3dc3cd
Škrbić Veljko <494716@student.fontys.nl>
2024-12-08 23:14:45 +0100

    Added K6 load testing, github workflow, and setup RabbitMQ

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./models');
+const startJobEventConsumer = require('./handlers/jobEventHandler');
+
+const app = express();
+const port = 5001;
+
+const jobReadRouter = require('./routers/JobsReadRouter');
+
+app.use(cors());
+app.use(express.json());
+app.use("/jobRead", jobReadRouter);
+
+(async () => {
+    try {
+        // Start the RabbitMQ job event consumer
+        await startJobEventConsumer();
+
+        // Sync the database and start the Express server
+        await db.sequelize.sync();
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Error during setup:", error);
+        process.exit(1);
+    }
+})();
---
+++ routers/JobsReadRouter.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const RabbitMQService = require('../rabbitMQService');
+const db = require('../models');
+
+router.get('/', async (req, res) => {
+    let jobs;
+    try{
+        jobs = await db.Job.findAll();
+    } catch(error){
+        console.error('Error getting jobs:', error);
+        return res.status(500).send('Error getting jobs');
+    }
+    // console.log('Jobs:', jobs);
+    return res.json(jobs);
+});
+
+// add a new route that get job by id
+router.get('/:id', async (req, res) => {
+    let job;
+    try{
+        job = await db.Job.findByPk(req.params.id);
+    } catch(error){
+        console.error('Error getting job:', error);
+        return res.status(500).send('Error getting job');
+    }
+    console.log('Job:', job);
+    return res.json(job);
+});
+
+module.exports = router;
