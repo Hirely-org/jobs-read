a312e559c44bf7fbda69fe46b39de7605df55589
Škrbić Veljko <494716@student.fontys.nl>
2024-12-09 10:16:53 +0100

    k6 test change

--- scripts/test.js
+++ scripts/test.js
@@ -3,13 +3,13 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '1s', target: 10000 },   // ramp up to 100 users
+        { duration: '1s', target: 1000 },   // ramp up to 100 users
         { duration: '1m', target: 0 },     // scale down to 0 users
     ],
 };
 
 export default function () {
-    const response = http.get('http://192.168.49.2:30002/jobRead');
+    const response = http.get('http://10.97.16.173/jobRead');
 
     check(response, {
         'status is 200': (r) => r.status === 200,
